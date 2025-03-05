a15bce90a2010d82cc923b68dc6374116551d9dd
santuzzxooy <santiagorochav@gmail.com>
2025-03-05 14:18:44 -0500

    iconos
    react-icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,14 @@ import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router'
+import { useLocation } from "react-router-dom";
+import { IoAccessibility, IoHomeSharp } from "react-icons/io5";
 import './style.css'
 import logo from './logo.svg'
 
 const Header = () => {
+  const location = useLocation();
+
   return (
     <Navbar>
       <Container>
@@ -22,9 +26,18 @@ const Header = () => {
             UT english blog
           </Link>
         </Navbar.Brand>
-        <Link to={"/UT_react_blog/members"}>
-          <Button variant="danger">Members</Button>
-        </Link>
+        {location.pathname === "/UT_react_blog" ? (
+          <Link to={"/UT_react_blog/members"}>
+            <Button variant="danger">
+              <IoAccessibility /> Members
+            </Button>
+          </Link>
+        ) : location.pathname === "/UT_react_blog/members" ? (
+          <Link to={"/UT_react_blog"}>
+            <Button variant="danger">
+              <IoHomeSharp /> Go home</Button>
+          </Link>
+        ) : null}
       </Container>
     </Navbar>
   )
