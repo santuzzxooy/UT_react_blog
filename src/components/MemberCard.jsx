0b27c3e1cd3f4ecde659e634be2ceab75087d5a9
santuzzxooy <santiagorochav@gmail.com>
2025-03-05 22:14:44 -0500

    casi listo

--- src/components/MemberCard.jsx
+++ src/components/MemberCard.jsx
@@ -1,6 +1,7 @@
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import './style.css'
 import Container from 'react-bootstrap/Container';
 
 
@@ -10,16 +11,38 @@ const MemberCard = () => {
       <Container>
         <Row>
           <Col>
-            <Card style={{ width: '18rem' }}>
+            <Card style={{ width: '22rem' }}>
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                <Card.Title>David Francisco Rodriguez Lopez</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Code: 085450332023</Card.Subtitle>
                 <Card.Text>
                   Some quick example text to build on the card title and make up the
                   bulk of the card's content.
                 </Card.Text>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '22rem' }}>
+              <Card.Body>
+                <Card.Title>Nelson Daniel Rodriguez Lopez</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Code: 085450132023</Card.Subtitle>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '22rem' }}>
+              <Card.Body>
+                <Card.Title>Josefth Santiago Rocha Villarraga</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Code: 085450722022</Card.Subtitle>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
               </Card.Body>
             </Card>
           </Col>
--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -1,11 +1,20 @@
 import Header from '../components/Header';
+import Carrusel from '../components/Carrusel';
+import Container from 'react-bootstrap/Container';
 import MemberCard from '../components/MemberCard';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import './style.css'
 
 const Members = () => {
   return (
     <div>
       <Header />
+      <Breadcrumb />
       <MemberCard />
+      <Breadcrumb />
+      <Container className='contenedor'>
+        <Carrusel />
+      </Container>
     </div>
   )
 }
